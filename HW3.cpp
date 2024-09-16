51c2b9b20b80e01af10474e61f81522cfbe13c98
Greelux <154830952+Greelux@users.noreply.github.com>
2024-09-16 18:06:58 +0300

    Add files via upload

---
+++ HW3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "\n\n";
+	cout << "\t\t\t\t\tSelling a Computer Desk!" << "\n";
+	cout << "\t\t\t\t\t180x90" << "\n";
+	cout << "\t\t\t\t\tTel: +487-77-77" << "\n";
+	cout << "\t\t\t\t\t777\t|77-77|" << "\n";
+	cout << "\t\t\t\t\t777\t|77-77|" << "\n";
+	cout << "\t\t\t\t\t777\t|77-77|" << "\n";
+	return 0;
+}
